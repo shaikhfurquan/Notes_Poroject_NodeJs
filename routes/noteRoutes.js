c89ddb5032e6784c736414abc9bfff116795b591
shaikhfurquan <shaikhfurquan313@gmail.com>
2023-11-03 13:09:17 +0530

    code added

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,5 +1,6 @@
 
 import express  from "express";
+import {noteModel} from "../models/noteModel.js"
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { authenticator } from "../middlewares/authenticator.js";
@@ -8,7 +9,25 @@ const noteRouter = express.Router();
 noteRouter.use(authenticator)
 
 //Making note router api
-noteRouter.get('/' , (req,res) =>{
+noteRouter.get('/' , async(req,res) =>{
+    //finding the data usind the user id with the help of headers token
+    let token = jwt.verify(token,"shaikh" , async(err,decode) =>{
+
+        try {
+            let data = await noteModel.find({user:decode.userId})
+            res.send({
+                data : data,
+                message : "success",
+                status : 1
+            })
+        } catch (error) {
+            res.send({
+                message: error.message,
+                status : 0
+            })
+        }
+    })
+
     res.send({
         message : "All the notes",
         status : 1
@@ -16,7 +35,22 @@ noteRouter.get('/' , (req,res) =>{
 
 })
 
-noteRouter.post('/create', (req,res) =>{
+
+//Route for creating the notes
+noteRouter.post('/create', async(req,res) =>{
+    try {
+        let note = new noteModel(req.body);
+        await note.save();
+        res.send({
+            message : "Note Created successfully",
+            status : 1
+        })
+    } catch (error) {
+        res.send({
+            message : "error.message",
+            status : 0
+        })
+    }
 
 })
 
